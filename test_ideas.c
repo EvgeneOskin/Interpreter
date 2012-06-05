3dd589843f640763e036e5e41d453b47d6eb8772
Evgene Oskin <burnst@mail.ru>
2012-06-05 13:18:49 +0700

    analyse input bracket (print error), exit with type "quit"

--- test_ideas.c
+++ test_ideas.c
@@ -1,24 +1,46 @@
 %top{
         int quit_INDEX = 0, num_vars = 0, num_bracket = 0;
-	int num_lines = 0, num_operations = 0;
+	int num_lines = 0, num_operations = 0, bracket_INDEX = 0;
+        void output_analyse();
 }
 %%
 \n     { 
-         printf ("\nyou input\nlines = %d,\noperations = %d,\nvars=%d,\nbrackets=%d\n\n",
-                        num_lines, num_operations, num_vars, num_bracket);
-         num_vars = 0;
-         num_bracket = 0;
-         num_lines = 0;
-         num_operations = 0;
+         if (bracket_INDEX != 0)
+         {
+             printf ("you have error in brackets\n");
+             return;
+         }
+         output_analyse();
         }
 "+"|"-"|"/"|"*"   ++num_operations;
-[a-pr-zA-Z]+    num_vars++;
-"("  num_bracket++;
-q    {
+"quit"    {
        printf ("i\'m quiting...\n");
        return; 
      }
+[a-zA-Z]+    num_vars++;
+"("  {
+      num_bracket++;
+      bracket_INDEX++;
+     }
+")"  {
+      bracket_INDEX--;
+      if (bracket_INDEX < 0)
+        {
+          printf ("you have error in brackets\n");
+          return;
+        }
+     }
+
 %%
+void output_analyse()
+{
+  printf ("\nyou input\nlines = %d,\noperations = %d,\nvars=%d,\nbrackets=%d\n\n",
+                        num_lines, num_operations, num_vars, num_bracket);
+  num_vars = 0;
+  num_bracket = 0;
+  num_lines = 0;
+  num_operations = 0;      
+}
 main()
 {
         yylex();
