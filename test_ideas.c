f7e37cad43975c27ab2cc245e9aca984cdd4ed2a
Evgene Oskin <burnst@mail.ru>
2012-06-05 12:51:43 +0700

    analyse how much input var-s and operators

--- test_ideas.c
+++ test_ideas.c
@@ -1,16 +1,25 @@
 %top{
+        int quit_INDEX = 0, num_vars = 0, num_bracket = 0;
 	int num_lines = 0, num_operations = 0;
 }
 %%
-\n      ++num_lines;
-"+"     |
-"-"     |
-"/"     |
-"*"     ++num_operations;
+\n     { 
+         printf ("\nyou input\nlines = %d,\noperations = %d,\nvars=%d,\nbrackets=%d\n\n",
+                        num_lines, num_operations, num_vars, num_bracket);
+         num_vars = 0;
+         num_bracket = 0;
+         num_lines = 0;
+         num_operations = 0;
+        }
+"+"|"-"|"/"|"*"   ++num_operations;
+[a-pr-zA-Z]+    num_vars++;
+"("  num_bracket++;
+q    {
+       printf ("i\'m quiting...\n");
+       return; 
+     }
 %%
 main()
 {
-	yylex();
-	printf( "\nlines = %d, operations = %d\n",
-	num_lines, num_operations );
+        yylex();
 }
